dbb00d8cb094fc4cb673f7343eb69cb86a4b514a
蓝月明宁 <112787698+lanyuemingning@users.noreply.github.com>
2026-02-28 11:57:05 +0800

    关卡修改
    This reverts commit fd3a3a3f18cefbd30c1a66152837957ce6d1a9ea.

--- Assets/Scripts/Water/UI/UIVictory.cs
+++ Assets/Scripts/Water/UI/UIVictory.cs
@@ -19,7 +19,7 @@ namespace Game.Water
         private SaveDataUtility saveDataUtility;
         [SerializeField] private Sprite[] unlockSprites;
         private readonly int[] UNLOCKLEVEL = new int[]
-            { 11, 21, 31, 41, 51, 61, 71, 81, 91, 101, 121, 141, 161, 181, 201, 221, 301, 401 };
+            { 3, 11, 21, 31, 41, 51, 61, 71, 81, 91, 101, 121, 141, 161, 181, 201, 301, 401 };
 
         public Material TMPFont_red;
         public IArchitecture GetArchitecture()
